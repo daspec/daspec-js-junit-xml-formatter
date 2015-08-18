16f33bb383f909773b71a1ba7d3b777f74bede77
Gojko Adzic <gojko@gojko.com>
2015-08-18 02:05:45 +0200

    naming tests

--- spec/junit-xml-formatter-spec.js
+++ spec/junit-xml-formatter-spec.js
@@ -1,10 +1,12 @@
-/*global describe, it, expect, beforeEach, os, require, afterEach */
+/*global describe, it, expect, beforeEach, require, afterEach, process */
 describe('Junit XML Formatter', function () {
 	'use strict';
-	var underTest = require('../junit-xml-formatter'),
+	var underTest = require('../src/junit-xml-formatter'),
 		observable = require('daspec-core/src/observable'),
+		libxmljs = require('libxmljs'),
 		path = require('path'),
 		fs = require('fs'),
+		os = require('os'),
 		config,
 		outputDir,
 		runner,
@@ -28,32 +30,31 @@ describe('Junit XML Formatter', function () {
 			'output-dir': outputDir
 		};
 		runner = observable({});
-		underTest(runner, config);
 	});
 	afterEach(function () {
 		rmrf(outputDir);
 	});
 	describe('configuration', function () {
 		var createSuite = function () {
+			underTest(runner, config);
 			runner.dispatchEvent('suiteEnded', {passed: 1, executed: 1});
 		};
 		it('throws an exception if output-dir is not set and the junit-xml-file-name is not provided', function () {
 			expect(function () {
 				config['output-dir'] = undefined;
-				createSuite();
-			}).toThrow('output-dir not set');
+				underTest(runner, config);
+			}).toThrowError('output-dir not set');
 		});
 		it('throws an exception if output-dir is not set and the junit-xml-file-name does not contain a folder', function () {
 			expect(function () {
 				config['output-dir'] = undefined;
 				config['junit-xml-file-name'] = 'test.xml';
-				createSuite();
-			}).toThrow('output-dir not set');
+				underTest(runner, config);
+			}).toThrowError('output-dir not set');
 		});
 		it('creates folders and the report.xml file if the junit-xml-file-name is not provided', function () {
 			createSuite();
 			expect(fs.existsSync(path.join(outputDir, 'report.xml'))).toBeTruthy();
-
 		});
 		it('ignores the output dir if the junit-xml-file-name contains a path separator', function () {
 			outputDir = path.join(os.tmpdir(), 'alternate-output-dir');
@@ -84,5 +85,138 @@ describe('Junit XML Formatter', function () {
 		});
 
 	});
+	describe('content', function () {
+		var readContents = function () {
+			var xml = fs.readFileSync(path.join(outputDir, 'report.xml'), {encoding: 'utf8'}),
+				parsed = libxmljs.parseXml(xml);
+			return parsed.root();
+		};
+		beforeEach(function () {
+			underTest(runner, config);
+		});
+		it('makes a testsuite root object with DaSpec as name', function () {
+			runner.dispatchEvent('suiteEnded', {passed: 1, executed: 5, failed: 2, error: 3});
+			var xmlRoot = readContents();
+			expect(xmlRoot.name()).toEqual('testsuite');
+			expect(xmlRoot.attr('name').value()).toEqual('DaSpec');
+		});
+		describe('total counts', function () {
+			it('ignores the counts from the suiteEnded event, but instead counts entire specs', function () {
+				runner.dispatchEvent('specEnded', 'specname', {passed: 5, executed: 5});
+				runner.dispatchEvent('suiteEnded', {passed: 1, executed: 5, failed: 2, error: 3});
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('tests').value()).toEqual('1');
+				expect(xmlRoot.attr('failures').value()).toEqual('0');
+				expect(xmlRoot.attr('errors').value()).toEqual('0');
+			});
+			it('considers specs with at least one failure but no errors as failed', function () {
+				runner.dispatchEvent('specEnded', 'specname', {passed: 3, executed: 5, failed: 2});
+				runner.dispatchEvent('suiteEnded');
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('tests').value()).toEqual('1');
+				expect(xmlRoot.attr('failures').value()).toEqual('1');
+				expect(xmlRoot.attr('errors').value()).toEqual('0');
+			});
+			it('considers specs with at least one error as errors, regardless of if they have a failure', function () {
+				runner.dispatchEvent('specEnded', 'specname', {passed: 3, executed: 4, failed: 0, error: 1});
+				runner.dispatchEvent('specEnded', 'specname2', {passed: 3, executed: 6, failed: 2, error: 1});
+				runner.dispatchEvent('suiteEnded');
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('failures').value()).toEqual('0');
+				expect(xmlRoot.attr('errors').value()).toEqual('2');
+			});
+			it('considers specs without errors or failures as skipped if there are any skipped steps', function () {
+				runner.dispatchEvent('specEnded', 'specname', {passed: 3, executed: 5, skipped: 1});
+				runner.dispatchEvent('suiteEnded');
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('tests').value()).toEqual('1');
+				expect(xmlRoot.attr('failures').value()).toEqual('0');
+				expect(xmlRoot.attr('errors').value()).toEqual('0');
+				expect(xmlRoot.attr('skipped').value()).toEqual('1');
+			});
+			it('considers specs without any executed steps as skipped', function () {
+				runner.dispatchEvent('specEnded', 'specname', {executed: 0});
+				runner.dispatchEvent('suiteEnded');
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('tests').value()).toEqual('1');
+				expect(xmlRoot.attr('skipped').value()).toEqual('1');
+			});
+			it('considers specs with failures as failed even if there are any skipped steps', function () {
+				runner.dispatchEvent('specEnded', 'specname', {passed: 3, executed: 5, skipped: 1, failed: 1});
+				runner.dispatchEvent('suiteEnded');
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('tests').value()).toEqual('1');
+				expect(xmlRoot.attr('failures').value()).toEqual('1');
+				expect(xmlRoot.attr('errors').value()).toEqual('0');
+			});
+			it('aggregates totals across multiple specs', function () {
+				runner.dispatchEvent('specEnded', 'specname', {passed: 4, executed: 4});
+				runner.dispatchEvent('specEnded', 'specname2', {passed: 3, executed: 6, failed: 2});
+				runner.dispatchEvent('specEnded', 'specname3', {passed: 3, executed: 6, failed: 2, error: 1});
+				runner.dispatchEvent('suiteEnded');
+				var xmlRoot = readContents();
+				expect(xmlRoot.attr('failures').value()).toEqual('1');
+				expect(xmlRoot.attr('errors').value()).toEqual('1');
+				expect(xmlRoot.attr('tests').value()).toEqual('3');
+			});
+		});
+		describe('individual spec results', function () {
+			describe('naming', function () {
+				var checkName = function (path, expectedName, expectedClassName) {
+					runner.dispatchEvent('specEnded', path, {passed: 4, executed: 4});
+					runner.dispatchEvent('suiteEnded');
+					var xmlRoot = readContents(),
+						specElement = xmlRoot.find('testcase')[0];
+					expect(specElement.attr('name').value()).toEqual(expectedName);
+					expect(specElement.attr('classname').value()).toEqual(expectedClassName);
+				};
+				it('uses the file name as the test name and the dir name as the class name', function () {
+					checkName('folder/subfolder/specname.md', 'specname', 'folder.subfolder');
+				});
+				it('survives non .md file names', function () {
+					checkName('folder/subfolder/specname.txt', 'specname', 'folder.subfolder');
+				});
+				it('survives names without extension', function () {
+					checkName('folder/subfolder/specname', 'specname', 'folder.subfolder');
+				});
+				it('survives names with quotes (xml escaping test)', function () {
+					checkName('fo"lder/subfold"er/sp"ecname', 'specname', 'folder.subfolder');
+				});
+				it('uses daspec as the classname if no folders provided', function () {
+					checkName('specname.md', 'specname', 'daspec');
+				});
+				it('removes starting / from absolute paths', function () {
+					checkName('/folder/subfolder/specname.md', 'specname', 'folder.subfolder');
+				});
+				it('removes starting . and .. from relative paths', function () {
+					checkName('./../folder/subfolder/specname.md', 'specname', 'folder.subfolder');
+				});
+				it('uses daspec as the class name if using .. and no subfolders', function () {
+					checkName('../specname.md', 'specname', 'daspec');
+				});
+				it('uses daspec as the class name if using . and no subfolders', function () {
+					checkName('./specname.md', 'specname', 'daspec');
+				});
+				it('uses daspec as the class name if using root files -- hopefully nobody is that stupid, but...', function () {
+					checkName('/specname.md', 'specname', 'daspec');
+				});
+				it('uses the extension as the test name if using files without names -- hopefully nobody is that stupid, but...', function () {
+					checkName('folder/.md', '.md', 'folder');
+				});
+			});
+		});
 
+	});
+	it('validates against XML expected by Jenkins', function () {
+		underTest(runner, config);
+		runner.dispatchEvent('specEnded', 'specname', {passed: 4, executed: 4});
+		runner.dispatchEvent('specEnded', 'specname2', {passed: 3, executed: 6, failed: 2});
+		runner.dispatchEvent('specEnded', 'specname3', {passed: 3, executed: 6, failed: 2, error: 1});
+		runner.dispatchEvent('suiteEnded');
+
+		// from https://svn.jenkins-ci.org/trunk/hudson/dtkit/dtkit-format/dtkit-junit-model/src/main/resources/com/thalesgroup/dtkit/junit/model/xsd/junit-4.xsd
+		var xsd = libxmljs.parseXml(fs.readFileSync(path.join(process.cwd(), 'spec', 'junit-4.xsd'), {encoding: 'utf8'})),
+			xml = libxmljs.parseXml(fs.readFileSync(path.join(outputDir, 'report.xml'), {encoding: 'utf8'}));
+		expect(xml.validate(xsd)).toBeTruthy();
+	});
 });
--- src/junit-xml-formatter.js
+++ src/junit-xml-formatter.js
@@ -1,8 +1,65 @@
 /*global module, require */
+var path = require('path'),
+	fs = require('fs'),
+	mkdirp = require('mkdirp');
 module.exports = function junitXmlFormatter(runner, config) {
 	'use strict';
+	var lines = [],
+		fileName,
+		executedSpecs = 0,
+		failedSpecs = 0,
+		errorSpecs = 0,
+		skippedSpecs = 0,
+		parseConfig = function () {
+			fileName = config['junit-xml-file-name'] || 'report.xml';
+			if (Array.isArray(fileName)) {
+				fileName = fileName[0];
+			}
+			if (fileName.indexOf(path.sep) < 0) {
+				if (!config['output-dir']) {
+					throw new Error('output-dir not set');
+				}
+				fileName = path.join(config['output-dir'], fileName);
+			}
+		},
+		saveSuiteFile = function () {
+			mkdirp.sync(path.dirname(fileName));
+			lines.unshift('<?xml version="1.0" encoding="UTF-8"?>');
+			fs.writeFileSync(fileName, lines.join('\n'));
+		},
+		testName = function (filePath) {
+			filePath = filePath.replace(/"/g, '');
+			return path.basename(filePath, path.extname(filePath));
+		},
+		className = function (filePath) {
+			filePath = path.normalize(filePath);
+			filePath = filePath.replace(/^\.+/, '').replace(/"/g, '');
+			if (path.isAbsolute(filePath)) {
+				filePath = filePath.substring(filePath.indexOf(path.sep) + 1);
+			}
+			if (filePath.indexOf(path.sep) < 0) {
+				return 'daspec';
+			}
+			return path.dirname(filePath).replace(path.sep, '.');
+		};
+	parseConfig();
 	runner.addEventListener('specEnded',  function (name, counts) {
+		executedSpecs++;
+		if (counts.error) {
+			errorSpecs++;
+		} else if (counts.failed > 0) {
+			failedSpecs++;
+		} else if (counts.skipped > 0 || !counts.executed) {
+			skippedSpecs++;
+		}
+		lines.push('<testcase name="' + testName(name) + '" classname="' + className(name) + '">');
+		lines.push('</testcase>');
 	});
-	runner.addEventListener('suiteEnded', function (counts) {
+	runner.addEventListener('suiteEnded', function () {
+		lines.unshift('<testsuite name="DaSpec" tests="' + executedSpecs + '"' +
+			' failures="' + failedSpecs + '" errors="' + errorSpecs + '"' +
+			' skipped="' + skippedSpecs + '">');
+		lines.push('</testsuite>');
+		saveSuiteFile();
 	});
 };
